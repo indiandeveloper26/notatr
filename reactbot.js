6f4fc66d37543ba81453b5cec432b589cbbe34c8
indiandeveloper26 <indiandeveloper25@gmail.com>
2025-05-24 09:37:44 -0700

    first final this is appserver

--- reactbot.js
+++ reactbot.js
@@ -377,6 +377,12 @@ io.on('connection', (socket) => {
   });
 });
 
+
+
+app.get("/trad",(req,res)=>{
+    res.send("this is trading server for apii liten 5000 port")
+})
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
